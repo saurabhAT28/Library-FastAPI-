8eec171a6ec161d69825aaba740640a735f81068
saurabhAT28 <85108331+saurabhAT28@users.noreply.github.com>
2023-01-31 16:29:47 +0530

    Add files via upload

---
+++ book/Hashing/hashed.py
@@ -0,0 +1,13 @@
+from passlib.context import CryptContext
+
+pwd_cxt=CryptContext(schemes=["bcrypt"],deprecated="auto")
+
+
+class Hash():
+    def bcrypt(password:str):
+        return pwd_cxt.hash(password)
+
+    def verify(hashed_pass,plain_pass):
+        return pwd_cxt.verify(hashed_pass,plain_pass)
+
+
